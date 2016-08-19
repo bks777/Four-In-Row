a867051d7cf33339aff433a993db04e2eb9f84d4
k.bokov <k.bokov@lucky-labs.com>
2016-08-19 15:41:01 +0300

    [main]refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
 "use strict";
-
 import Loader from './components/core/Loader';
 import App from './components/app/Application';
 
 let FIR,
-    promise = Loader.httpGet({url:'../config/config.json'})
+    promise = Loader.httpGet({url:'./config/config.json'})
         .then((config)=>{
-            window.FIR = new App(JSON.parse(config), PIXI);
+            FIR = new App(JSON.parse(config), PIXI);
         });
-// window.FIR = FIR;
